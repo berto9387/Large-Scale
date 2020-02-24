2e8109bfc13afa3ed621ea6e2b2a61f620f3f505
berto9387 <berto9387@gmail.com>
2020-02-24 11:07:01 +0100

    gestione membri squadra fxml

---
+++ task2/markapp/src/main/java/Controller/GestioneMembriSquadraController.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+/**
+ *
+ * @author berto
+ */
+public class GestioneMembriSquadraController {
+    @FXML
+    private Text nomeSquadra;
+
+    @FXML
+    private Text errorScegliSquadra;
+
+    @FXML
+    private Text ruoloTeamSquadra;
+
+    @FXML
+    private Text nomeTeamSquadra;
+
+    @FXML
+    private Text emailTeamSquadra;
+
+    @FXML
+    private JFXTextField emailInput;
+
+    @FXML
+    private Text errorCambiaAmministratoreSquadra;
+
+    @FXML
+    void cercaTeamSquadra(ActionEvent event) {
+
+    }
+
+    @FXML
+    void modificaTeamSquadra(MouseEvent event) {
+
+    }
+}
--- task2/markapp/src/main/java/Controller/HomePageAmministratoreDiSquadraController.java
+++ task2/markapp/src/main/java/Controller/HomePageAmministratoreDiSquadraController.java
@@ -5,11 +5,40 @@
  */
 package Controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import task2.markapp.ScreenController;
+
 /**
  *
  * @author berto
  * usa GestioneProfiliMongoDb
  */
 public class HomePageAmministratoreDiSquadraController extends GenerallController{
+    @FXML
+    private BorderPane mainPane;
+
+    @FXML
+    private ImageView imgAdmin;
+
+    @FXML
+    private Label nomeAdmin;
+
+    @FXML
+    private Label emailAdmin;
+
     
+
+    @FXML
+    void handleGestioneMembriSquadra(MouseEvent event) {
+        ScreenController.showPage("GestioneMembriSquadra");
+    }
+
+    @FXML
+    void handleModificaProfilo(MouseEvent event) {
+        ScreenController.showPage("ModificaProfiloPersonale");
+    }
 }
