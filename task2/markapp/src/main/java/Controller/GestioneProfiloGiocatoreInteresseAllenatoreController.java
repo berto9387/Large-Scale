fdc4faa039ba1496804110ed4fb013a954d3f5de
Gianluca <tummi1905@gmail.com>
2020-02-28 21:32:46 +0100

    Parte grafica dei profili d'interesse e inizio controller

---
+++ task2/markapp/src/main/java/Controller/GestioneProfiloGiocatoreInteresseAllenatoreController.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.*;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.layout.*;
+/**
+ * FXML Controller class
+ *
+ * @author Gianluca
+ */
+public class GestioneProfiloGiocatoreInteresseAllenatoreController implements Initializable {
+    
+    @FXML
+    private VBox areaModificaProfili;
+    @FXML
+    private ChoiceBox sceltaRuolo;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
