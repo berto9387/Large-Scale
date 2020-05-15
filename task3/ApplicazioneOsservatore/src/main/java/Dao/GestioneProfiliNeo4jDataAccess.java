2b503e7c5c53d6f422447cd165c63338b4a089c4
Gianluca <tummi1905@gmail.com>
2020-05-15 22:20:33 +0200

    Aggiunte altre funzioni DAO per la gestione di un profilo

--- task3/ApplicazioneOsservatore/src/main/java/Dao/GestioneProfiliNeo4jDataAccess.java
+++ task3/ApplicazioneOsservatore/src/main/java/Dao/GestioneProfiliNeo4jDataAccess.java
@@ -50,7 +50,7 @@ public class GestioneProfiliNeo4jDataAccess extends Neo4jDataAccess {
      * @param id
      * @return 
      */
-    public static int transactionEliminaAccount(Transaction tx,String email, String id){
+    private static int transactionEliminaAccount(Transaction tx,String email, String id){
         HashMap<String,Object> parameters =new HashMap<>();
         parameters.put("email",email);
         parameters.put("<id>", id);
@@ -94,7 +94,7 @@ public class GestioneProfiliNeo4jDataAccess extends Neo4jDataAccess {
       * null se non è presente nel database un utente con email corrispondente
       * al parametro attuale.
       */
-    public static Utente transactionCercaUtenteDaEmail(Transaction tx,String email)
+    private static Utente transactionCercaUtenteDaEmail(Transaction tx,String email)
     {   
         
         Utente utente=null;
@@ -110,7 +110,7 @@ public class GestioneProfiliNeo4jDataAccess extends Neo4jDataAccess {
         String ruolo="NA";        
         StatementResult result=tx.run("MATCH(a:Utente) WHERE a.email=$email"+
                  " OPTIONAL MATCH (a)-[r:Tesserato_per]->(s:Societa) OPTIONAL MATCH (so:Societa) WHERE (so)<-[r]-(a)"+
-                  "RETURN a so",parameters);
+                  "RETURN a,so",parameters);
         if(!result.hasNext())
             return null;
         Record record=result.single();
@@ -124,6 +124,136 @@ public class GestioneProfiliNeo4jDataAccess extends Neo4jDataAccess {
                 ruolo = value.get("ruolo",ruolo);
             }
             if ("so".equals(nameValue.key())) { 
+                Value value = nameValue.value();
+                nomeSocieta=record.get("nomeSocieta",nomeSocieta);
+                nazione=record.get("nazione",nazione);
+                idSocieta=record.get("id",idSocieta);
+            }         
+        }
+        if(idSocieta!="NA")
+        {
+            societa= new Societa(idSocieta, nomeSocieta,nazione, idUtente);
+        }   
+        utente= new Utente(idUtente,nome,cognome,email,ruolo,societa);
+        return utente;
+    }
+    /**
+     * 
+     * @param tx
+     * @param vecchiaEmail
+     * @param nuovaEmail
+     * @param nuovaPassword
+     * @return 
+     */
+    private static boolean transactionModificaProfilo(Transaction tx,String vecchiaEmail,String nuovaEmail,String nuovaPassword)
+    {   
+        
+        Utente user=cercaUtenteDaEmail(vecchiaEmail); 
+        if ((user==null)||(nuovaEmail.equals("")&&nuovaPassword.equals(""))){
+            return false;
+        } 
+        String query="MATCH(utente:Utente) WHERE utente.email=$email SET";       
+        HashMap<String,Object> parameters =new HashMap<>();
+        String giunzione=" ";
+        if(!nuovaEmail.equals(""))
+        {
+           query=query + " utente.email=$nuovaEmail";
+           parameters.put("nuovaEmail", nuovaEmail);
+           giunzione=",";
+        }
+        if(!nuovaPassword.equals(""))
+        {
+            query=query+giunzione+"utente.password=$nuovaPassword";
+            parameters.put("nuovaPassword", nuovaPassword);
+        }
+        parameters.put("email",vecchiaEmail);
+        StatementResult result=tx.run(query,parameters);
+        
+        return true;
+    }
+    /**
+     * 
+     * @param vecchiaEmail
+     * @param nuovaEmail
+     * @param nuovaPassword
+     * @return 
+     */
+    public static boolean modificaProfilo(final String vecchiaEmail,final String nuovaEmail,final String nuovaPassword)
+    {
+        try(Session session=driver.session())
+        {
+            
+            return session.readTransaction(new TransactionWork<Boolean>()
+            {
+                @Override
+                public Boolean execute(Transaction tx)
+                {
+                   return transactionModificaProfilo(tx,vecchiaEmail,nuovaEmail,nuovaPassword);
+                }
+            });
+        } 
+    }
+    /**
+     * 
+     * @param nomeSquadra
+     * @param nazioneSquadra
+     * @return 
+     */
+    public static Utente cercaProfiloUtenteDaSocieta(final String nomeSquadra,final String nazioneSquadra)
+    {
+       try(Session session=driver.session())
+        {
+            
+            return session.readTransaction(new TransactionWork<Utente>()
+            {
+                @Override
+                public Utente execute(Transaction tx)
+                {
+                   return transactionCercaProfiloUtenteDaSocieta(tx,nomeSquadra,nazioneSquadra);
+                }
+            });
+        }
+    }
+    /**
+     * 
+     * @param tx
+     * @param nomeSquadra
+     * @param nazioneSquadra
+     * @return 
+     */
+    private static Utente transactionCercaProfiloUtenteDaSocieta(Transaction tx,String nomeSquadra,String nazioneSquadra)
+    {
+                
+        Utente utente=null;
+        Societa societa = null;
+        HashMap<String,Object> parameters =new HashMap<>();
+        parameters.put("nomeSocieta",nomeSquadra);
+        parameters.put("nazioneSocieta",nazioneSquadra);
+        String nomeSocieta="NA";
+        String nazione="NA";
+        String idSocieta="NA";
+        String nome="NA";
+        String cognome="NA";
+        String idUtente ="NA";
+        String ruolo="NA";
+        String email = "NA";
+        StatementResult result=tx.run("MATCH ((user:Utente)-[r:Tesserato_per]->(soc:Societa))"+""
+                + " WHERE soc.nomeSocieta=$nomeSocieta "
+                + "AND soc.nazione=$nazioneSocieta  RETURN user,soc",parameters);
+        if(!result.hasNext())
+            return null;
+        Record record=result.single();
+        List<Pair<String,Value>> values = record.fields();
+        for (Pair<String,Value> nameValue: values) {
+            if ("user".equals(nameValue.key())) { 
+                Value value = nameValue.value();
+                nome = value.get("nome",nome);
+                cognome= value.get("cognome",cognome);
+                idUtente = value.get("<id>",idUtente);
+                ruolo = value.get("ruolo",ruolo);
+                email=value.get("email", email);
+            }
+            if ("soc".equals(nameValue.key())) { 
                 Value value = nameValue.value();
                 nomeSocieta=record.get("nomeSocieta",nomeSocieta);
                 nazione=record.get("nazione",nazione);
