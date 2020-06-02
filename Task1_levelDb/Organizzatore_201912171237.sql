e93685ddc8267dd289c9124588bfcaf3d7ed2333
pcal93 <56187935+pcal93@users.noreply.github.com>
2020-06-02 12:02:44 +0200

    dump add

---
+++ Task1_levelDb/Organizzatore_201912171237.sql
@@ -0,0 +1,3 @@
+INSERT INTO task_1.Organizzatore (id_Organizzatore,nome,cognome,data_nascita,email,password,username,phone) VALUES 
+(1,'prova','zizza','1990-12-12','ccc@sss.com','pippo','pippo','0988')
+;
